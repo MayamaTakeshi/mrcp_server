e858a4ccff1142f5d30ceb9222fd2a50db9259bb
MayamaTakeshi <mayamatakeshi@gmail.com>
2021-02-14 11:22:03 +0900

    Removed data-matching

--- src/actors/sip_server.js
+++ src/actors/sip_server.js
@@ -6,7 +6,6 @@ const uuid_v4 = require('uuid').v4
 const Deque = require('collections/deque')
 const _ = require('lodash')
 const mrcp_utils = require('mrcp-utils')
-const dm = require('data-matching')
 
 const logger = require('../logger.js')
 const u = require('../utils.js')
@@ -22,45 +21,6 @@ const FILE = u.filename()
 const log = (line, level, entity, msg) => {
     logger.log(level, entity, `(${FILE}:${line}) ${msg}`)
 }
-
-/*
-const sdp_matcher = dm.partial_match({
-	connection: { ip: dm.collect('remote_rtp_ip') },
-	media: dm.unordered_list([
-		{
-			type: 'application',
-			port: 9,
-			protocol: 'TCP/MRCPv2',
-			payloads: ["1"],
-			resource: dm.collect('resource'),
-			connection: dm.collect('connection'),
-		},
-		{
-			type: 'audio',
-			port: dm.collect('remote_rtp_port'),
-			protocol: 'RTP/AVP',
-			payloads: dm.collect("rtp_payloads"),
-		}
-	])
-})
-
-var gen_sdp = (local_ip, mrcp_port, rtp_port, connection, uuid, resource) => {
-	return 'v=0\r\n' +
-	`o=mrcp_server 1212606071011504954 4868540303632141964 IN IP4 ${local_ip}\r\n` +
-	"s=-\r\n" +
-	`c=IN IP4 ${local_ip}\r\n` +
-	't=0 0\r\n' +
-	`m=application ${mrcp_port} TCP/MRCPv2 1\r\n` +
-	'a=setup:passive\r\n' +
-	`a=connection:${connection}\r\n` +
-	`a=channel:${uuid}@${resource}\r\n` +
-	'a=cmid:1\r\n' +
-	`m=audio ${rtp_port} RTP/AVP 0\r\n` +
-	'a=rtpmap:0 PCMU/8000\r\n' +
-	`a=${resource == 'speechsynth' ? 'sendonly' : 'recvonly'}\r\n` +
-	'a=mid:1\r\n'
-}
-*/
 
 var rstring = () => {
 	return Math.floor(Math.random()*1e6).toString()
